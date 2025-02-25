81bfc1860c0d291baf9a5dec7bb513c2bc818710
Antonio De Angelis <deangelis.antonio122@gmail.com>
2025-02-25 18:02:49 +0100

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,18 +5,7 @@ import {Router, RouterModule, RouterOutlet} from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterModule, RouterOutlet],
-  template: `
-    <main>
-      <a [routerLink]="['/']">
-        <header class="brand-name">
-          <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
-        </header>
-      </a>
-      <section class="content">
-        <router-outlet></router-outlet>
-      </section>
-    </main>
-  `,
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
