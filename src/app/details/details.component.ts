0142b3a5ac8189c304f9b5a4ae1005a90846b18d
Antonio De Angelis <deangelis.antonio122@gmail.com>
2025-03-11 15:21:24 +0100

    Gave the form a feedback
    Form v1

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -3,7 +3,7 @@ import {CommonModule} from '@angular/common';
 import {ActivatedRoute} from '@angular/router';
 import {HousingService} from '../housing.service';
 import {HousingLocation} from '../housing-location/housing-location.component';
-import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 
 
 @Component({
@@ -16,10 +16,13 @@ export class DetailsComponent {
   route: ActivatedRoute = inject(ActivatedRoute);
   housingService = inject(HousingService);
   housingLocation: HousingLocation | undefined;
+  isLoading = false;
+  isSuccessful = false;
+  isError = false;
   applyForm = new FormGroup({
-    firstName: new FormControl(''),
-    lastName: new FormControl(''),
-    email: new FormControl(''),
+    firstName: new FormControl('', [Validators.required]),
+    lastName: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
   });
   constructor() {
     const housingLocationId = parseInt(this.route.snapshot.params['id'], 10);
@@ -27,12 +30,25 @@ export class DetailsComponent {
       this.housingLocation = housingLocation;
     });
   }
-  submitApplication() {
+  submitApplication() { // POST Simulation
+    this.isLoading = true;
+    this.isSuccessful = false;
+    this.isError = false;
+
     this.housingService.submitApplication(
       this.applyForm.value.firstName ?? '',
       this.applyForm.value.lastName ?? '',
       this.applyForm.value.email ?? '',
     );
-    this.applyForm.reset();
-  }
+
+    // Simulate an async operation
+    setTimeout(() => {
+      this.isLoading = false;
+      const success = Math.random() > 0.5; // Simulate success or failure
+      if (success) {
+        this.isSuccessful = true;
+      } else {
+        this.isError = true;
+      }
+    }, 2000);  }
 }
